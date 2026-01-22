dcc528eb988bccb27b3ce1be4fedf42ac420dd6f
adarsh-rafay <adarsh@rafay.co>
2026-01-22 11:23:08 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.29"
+}
+
+variable "node_instance_type" {
+  description = "Instance type for worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
