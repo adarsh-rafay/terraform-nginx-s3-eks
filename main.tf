2596280a8f4170fdc1f5b82cb7e2f9636ce5a002
adarsh-rafay <adarsh@rafay.co>
2026-01-22 11:24:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+# VPC
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+
+# EKS Cluster
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = [var.node_instance_type]
+
+      desired_size = var.desired_capacity
+      min_size     = 1
+      max_size     = 3
+    }
+  }
+}
